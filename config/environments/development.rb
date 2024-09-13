59c43e39d06497a160a31b4a2aba41b4c2295186
GUSTTAVO JAGUAR <71243629+GusttavoJaguar@users.noreply.github.com>
2024-09-13 19:48:01 -0300

    Jaguar News

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,18 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.example.com",
+    port: 587,
+    user_name: "username",
+    password: "password",
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 end
