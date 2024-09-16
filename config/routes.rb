5e43b289b5d329a358f281051f97ee2f6db747e5
GUSTTAVO JAGUAR <71243629+GusttavoJaguar@users.noreply.github.com>
2024-09-15 22:31:59 -0300

    Jaguar News

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # Define a rota raiz para o index do controlador Posts
+  root "posts#index"
 
   resources :posts do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Define rotas aninhadas para comentários dentro de posts
     resources :comentarios, only: [:new, :create]
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # Defines the root path route ("/")
-  # root "posts#index"
   end
+
+  # Outras rotas, se houver
 end
